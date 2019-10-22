baeaa6c3a3a080f2e9c99798bbcdb8dfc11d8e12
bethbyang <7730391@qq.com>
2019-10-22 23:05:58 +0800

    first commit

---
+++ mall_web/src/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    loginuser:'',
+    prodlist:[]
+  },
+  mutations: {
+    setLoginUser(state,obj){
+      state.loginuser = obj;
+    },
+    setProdList(state,obj){
+      state.prodlist = [];
+      state.prodlist = obj;
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ mall_web/src/views/Details.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="details">
+        <img src="" alt="">
+        <div>
+            <p>产品名称：</p>
+            <p>产品价格：</p>
+            <p>详细说明：</p>
+            <p><Button type="primary" class="btn">点击购买</Button></p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Details"
+    }
+</script>
+
+<style scoped>
+
+</style>
